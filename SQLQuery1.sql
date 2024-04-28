f541bcdaf6047072650f8ea1c90d693b8f0929ce
Juan Pablo Bedoya <juan.pablo.bedoya02@gmail.com>
2024-04-27 21:34:39 -0500

    CambiosQuery

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,6 +10,8 @@ contraseñaUsuario varchar(50)
 
 insert into usuarios values ('admin','admin')
 
+update usuarios set contraseñaUsuario = 'hola' where nombreUsuario = 'juancho'
+
 select * from usuarios 
 
 delete from usuarios
