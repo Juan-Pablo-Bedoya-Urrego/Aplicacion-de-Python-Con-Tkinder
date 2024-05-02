c17598c6322d08042d67ae377db241b3cf2041bc
Juan Pablo Bedoya <juan.pablo.bedoya02@gmail.com>
2024-05-01 21:40:59 -0500

    cambios Query

--- src/DataBase/SQLQuery1.sql
+++ src/DataBase/SQLQuery1.sql
@@ -8,10 +8,31 @@ nombreUsuario varchar(50),
 contraseñaUsuario varchar(50)
 )
 
+create table moto(
+idMoto int primary key identity(1,1),
+placa varchar(50),
+nombre varchar(50))
+
+create table carro(
+idCarro int primary key identity(1,1),
+placa varchar(50),
+nombre varchar(50))
+
+create table placasVetadas(
+idVetada int primary key identity(1,1),
+placa varchar(50))
+
 insert into usuarios values ('admin','admin')
 
 update usuarios set contraseñaUsuario = 'hola' where nombreUsuario = 'juancho'
 
+delete from moto where placa = 'hhh-333'
+
 select * from usuarios 
+select * from moto
+select * from carro
+select placa from placasVetadas 
 
 delete from usuarios
+delete from moto
+delete from carro
